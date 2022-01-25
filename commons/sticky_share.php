1ffb53aa4eb5d7952f098e55383a42d3b0375842
Diki Siswanto <dickyachmady@gmail.com>
2022-01-25 18:11:39 +0800

    efisienkan sticky share

--- commons/sticky_share.php
+++ commons/sticky_share.php
@@ -1,3 +1,39 @@
+<?php  defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+<?php 
+  $social_media = [
+    'facebook' => [
+      'color' => 'bg-blue-700',
+      'icon' => 'fa-facebook-f',
+      'link' => 'https://facebook.com/sharer.php?u='
+    ],
+    'twitter' => [
+      'color' => 'bg-blue-400',
+      'icon' => 'fa-twitter',
+      'link' => 'https://twitter.com/share?url='
+    ],
+    'whatsapp' => [
+      'color' => 'bg-green-500',
+      'icon' => 'fa-whatsapp',
+      'link' => 'https://api.whatsapp.com/send?text='
+    ],
+    'telegram' => [
+      'color' => 'bg-blue-600',
+      'icon' => 'fa-telegram',
+      'link' => 'https://telegram.me/share/url?url='
+    ],
+    'pinterest' => [
+      'color' => 'bg-red-500',
+      'icon' => 'fa-pinterest',
+      'link' => 'https://pinterest.com/pin/create/link/?url='
+    ],
+    'messenger' => [
+      'color' => 'bg-blue-500',
+      'icon' => 'fa-facebook-messenger',
+      'link' => 'https://facebook.com/dialog/send?link='
+    ]
+  ];
+?>
 <section 
   class="fixed bottom-0 lg:top-1/2 left-0 transform transition-all duration-300 right-0 lg:right-auto z-30 lg:-translate-y-1/2"
   x-data="{
@@ -8,11 +44,8 @@
   @scroll.window="onScroll()"
   :class="{'invisible opacity-0 translate-y-full lg:-translate-y-1/2 lg:-translate-x-full': !stickyShare, 'visible opacity-100 translate-y-0 lg:translate-x-0 lg:-translate-y-1/2 z-50': stickyShare}">
   <ul class="bg-white lg:bg-transparent py-3 px-3 lg:pl-0 rounded-tr-lg rounded-br-lg text-center lg:text-left">
-    <li class="inline-block lg:block"><a href="https://facebook.com/sharer.php?u=<?= current_url() ?>" target="_blank" rel="noreferrer noopener" class="w-10 hover:relative hover:w-16 transition-all duration-300 h-10 bg-blue-700 text-white text-lg inline-flex items-center justify-center" aria-label="Bagikan ke Facebook"><i class="fab fa-facebook-f"></i></a></li>
-    <li class="inline-block lg:block"><a href="https://twitter.com/share?url=<?= current_url() ?>" target="_blank" rel="noreferrer noopener" class="w-10 hover:relative hover:w-16 transition-all duration-300 h-10 bg-blue-400 text-white text-lg inline-flex items-center justify-center" aria-label="Bagikan ke Twitter"><i class="fab fa-twitter"></i></a></li>
-    <li class="inline-block lg:block"><a href="https://api.whatsapp.com/send?text=<?= current_url() ?>" target="_blank" rel="noreferrer noopener" class="w-10 hover:relative hover:w-16 transition-all duration-300 h-10 bg-green-500 text-white text-lg inline-flex items-center justify-center" aria-label="Bagikan ke WhatsApp"><i class="fab fa-whatsapp"></i></a></li>
-    <li class="inline-block lg:block"><a href="https://telegram.me/share/url?url=<?= current_url() ?>" target="_blank" rel="noreferrer noopener" class="w-10 hover:relative hover:w-16 transition-all duration-300 h-10 bg-blue-600 text-white text-lg inline-flex items-center justify-center" aria-label="Bagikan ke Telegram"><i class="fab fa-telegram"></i></a></li>
-    <li class="inline-block lg:block"><a href="https://pinterest.com/pin/create/link/?url=<?= current_url() ?>" target="_blank" rel="noreferrer noopener" class="w-10 hover:relative hover:w-16 transition-all duration-300 h-10 bg-red-500 text-white text-lg inline-flex items-center justify-center" aria-label="Bagikan ke Pinterest"><i class="fab fa-pinterest"></i></a></li>
-    <li class="inline-block lg:block"><a href="https://facebook.com/dialog/send?link=<?= current_url() ?>" target="_blank" rel="noreferrer noopener" class="w-10 hover:relative hover:w-16 transition-all duration-300 h-10 bg-blue-500 text-white text-lg inline-flex items-center justify-center" aria-label="Bagikan ke Facebook Messenger"><i class="fab fa-facebook-messenger"></i></a></li>
+    <?php foreach($social_media as $key => $data) : ?>
+      <li class="inline-block lg:block"><a href="<?= $data['link'] . current_url() ?>" target="_blank" rel="noreferrer noopener" class="w-10 hover:relative hover:w-16 transition-all duration-300 h-10 text-white text-lg inline-flex items-center justify-center <?= $data['color'] ?>" aria-label="Bagikan ke <?= ucfirst($key) ?>" title="Bagikan ke <?= ucfirst($key) ?>"><i class="fab <?= $data['icon'] ?>"></i></a></li>
+    <?php endforeach ?>
   </ul>
 </section>
